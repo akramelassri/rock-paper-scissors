918b4e25f50e899f70290db166ac6462e89064af
akram <168238434+akramelassri@users.noreply.github.com>
2025-08-30 14:33:58 +0100

    Adding logic of getting human and computer choices

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
+'use strict';
+
 console.log("Hello this is a test");
+
+console.log(getHumanChoice());
+
+
+function getComputerChoice() {
+    let randomNumber = Math.floor(Math.random() * 3 + 1);
+
+    switch (randomNumber) {
+        case 1:
+            return 'rock';
+            break;
+        case 2:
+            return 'paper';
+            break;
+        case 3:
+            return 'scissors';
+            break;
+    }
+}
+
+function getHumanChoice() {
+    return prompt('rock, paper, scissors?','').toLowerCase();
+}
